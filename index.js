c64155b239d5d107f8fa4cad29564b5acb86452b
Bogdan <bog2k3@gmail.com>
2021-09-21 17:56:35 +0300

    fixed imports/exports

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import * as dosemu from "./src/dosemu.js";
 import * as dosemuSound from "./src/dosemu-sound.js";
 import * as dosemuSprite from "./src/dosemu-sprite.js";
-import * as dosemuBBox from "./src/dosemu-bbox";
+import * as dosemuBBox from "./src/dosemu-bbox.js";
 
-export default {
+export {
 	dosemu,
 	dosemuSound,
 	dosemuSprite,
